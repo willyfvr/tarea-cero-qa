031617304f29d962c8a13416ebfa609102d4f28b
Victor Fattor <victor.fattor@bosonic.digital>
2026-09-06 02:07:05 -0300

    add test.fixme to register a new user

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tarea Cero QA Automation Suite 
 
+## Automation Playwright tests for [Tarea Cero](https://tareacero.interaad.com.ar/) [Test Environment]
+
 Last report generated on Github Actions [REPORT](https://willyfvr.github.io/tarea-cero-qa/)
---
+++ tests/registration.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.fixme('Register new user with valid information.', async ({page}) => {
+  // STEPS TO TEST
+  // 1- access to site
+  // 2- press "Registrate ahora" link
+  // 3- check the navigation to registration form
+  // 4- fill fields with the correct information (credentials)
+  // 5- press submit button "CONTINUAR" 
+
+})
